4f6feb332272435924d914188d721aca4cb5c5e9
HamedKh99 <goo.hamedacc@gmail.com>
2019-03-06 17:38:44 +0330

    changed nums in main 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int x = 12;
         int y = 10;
-        int z = 0;
-        System.out.println(x + y + z);
+        int z = 20;
+        System.out.println(x + y + z / 2);
     }
 }
